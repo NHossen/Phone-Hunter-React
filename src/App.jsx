8e51db3a90a17ce4d6c3757248651a3399437d3a
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-09-21 17:03:46 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <h1 className='text-5xl text-[red] text-center'>Hello </h1>
+    <button className="btn btn-warning text-center"></button>
+
+       
+    </>
+  )
+}
+
+export default App
